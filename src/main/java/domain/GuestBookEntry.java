98d6ced3c99913ab2a972f735d9a60642a74e09f
Joemc96 <joe.mcn19@hotmail.com>
2018-04-10 13:07:29 +0100

    Updated spring version in dom file

--- src/main/java/domain/GuestBookEntry.java
+++ src/main/java/domain/GuestBookEntry.java
@@ -1,10 +1,11 @@
 package domain;
 
+import org.hibernate.validator.constraints.NotEmpty;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotEmpty;
 
 @Entity
 public class GuestBookEntry {
